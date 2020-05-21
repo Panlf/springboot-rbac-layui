c293ec2759b6a7d0e4614ebd8c7de3a49c4654e1
panlf <liangfeng_pan@163.com>
2020-05-21 21:52:28 +0800

    first commit

---
+++ src/main/java/com/plf/rbac/RbacLayUIApplication.java
@@ -0,0 +1,11 @@
+package com.plf.rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacLayUIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RbacLayUIApplication.class);
+    }
+}
